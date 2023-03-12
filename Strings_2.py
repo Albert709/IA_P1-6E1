4b07e1e75f90541f0e4139eadd64f3f22f2b20ab
Albert709 <a20310444@ceti.mx>
2023-03-12 14:54:28 -0600

    Práctica 3

---
+++ Strings_2.py
@@ -0,0 +1,22 @@
+x = "Probando " + "los strings"
+
+print(x)
+
+pp = "Hola "
+sp = "Mundo"
+pf = pp + sp
+
+print(pf)
+
+print(pp + "Mundo")
+
+nombre = "José Alberto"
+apellido = " Aguiar Hernández"
+nom_completo = nombre + apellido
+
+print(nom_completo)
+
+a = '23'
+b = '12'
+r = a + b
+print(r)
