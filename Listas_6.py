3aff0817219a20e7de7d16fdddb129d9671a8c8a
Albert709 <a20310444@ceti.mx>
2023-03-12 15:59:11 -0600

    Práctica 14

---
+++ Listas_6.py
@@ -0,0 +1,6 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print(colores)
+#agregar colores
+colores.append('fuxia')
+colores.append('celeste')
+print(colores)
