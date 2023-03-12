05659c61106caf0d6b6ef0b934e5279fa3e989ad
Albert709 <a20310444@ceti.mx>
2023-03-12 15:48:06 -0600

    Práctica 10

---
+++ Listas_2.py
@@ -0,0 +1,7 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+print("Color ", colores[-1])
+print("Color ", colores[-7])
+print("Color ", colores[-5])
+print("Color ", colores[-2])
+print("Color ", colores[-10])
