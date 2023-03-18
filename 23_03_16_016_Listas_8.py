b8a47bfb9e7997f40e2735e5d205902186f86793
Albert709 <a20310444@ceti.mx>
2023-03-18 02:23:45 -0600

    Práctica 16

---
+++ 23_03_16_016_Listas_8.py
@@ -0,0 +1,7 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+#ordenamos alfabéticamente descendente
+colores.sort(reverse=True)
+
+print(sorted(colores))#sorted es para que no afecte el nuevo posicionamiento de los elementos
+print(colores)
