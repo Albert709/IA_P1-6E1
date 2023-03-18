93a943e1d2624cf02efacafd19ad582c45f95186
Albert709 <a20310444@ceti.mx>
2023-03-18 02:23:38 -0600

    Práctica 15

---
+++ 23_03_16_015_Listas_7.py
@@ -0,0 +1,7 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+print(colores)
+#insertamos el color magenta
+colores.insert(-4, 'magenta')
+#insertamos el color turquesa
+colores.insert(-1, 'turquesa')
+print(colores)
