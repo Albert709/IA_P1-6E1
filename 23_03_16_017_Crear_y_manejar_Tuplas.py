4ca87308ddd7cd731911aaf050b5406166f3013e
Albert709 <a20310444@ceti.mx>
2023-03-18 02:27:47 -0600

    Práctica 17

---
+++ 23_03_16_017_Crear_y_manejar_Tuplas.py
@@ -0,0 +1,8 @@
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')#esto es una tupla
+print("La posición 2 contiene: ", colores[2])
+
+num = (10, 1, 5, 11)
+#operamos para obtener el resultado de 25
+operacion = num[0] + num[2] - num[1] + num[-1]
+
+print(operacion)
