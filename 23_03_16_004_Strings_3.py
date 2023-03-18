c10c23be0a254d3fdbfcda063ac572577e2b5a96
Albert709 <a20310444@ceti.mx>
2023-03-18 02:22:15 -0600

    Práctica 4

---
+++ 23_03_16_004_Strings_3.py
@@ -0,0 +1,8 @@
+nombre = "enrique barros fernández"
+print(nombre.title())
+
+frase = "Esta Es Una Frase Para Ser Formateada".lower()
+print(frase)
+
+frase = "Esta Es Una Frase Para Ser Formateada".upper()
+print(frase)
